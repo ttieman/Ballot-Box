5fb764768d3830febc9938a86f4c967cf9d2282c
ttieman <tiemantanner@gmail.com>
2023-04-04 20:49:42 -0700

    userRoutes for logging in, out, and signing up are prepared some basic debug routing for production enviorment to be removed before deployment

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
+
 const { User } = require('../../models');
 
 //POST /api/users/signup
@@ -46,35 +48,77 @@ router.post('/logout', (req, res) => {
 
 // POST /api/users/login
 //will reroute to /
-router.post('/login', async (req, res) => {
+router.post('/login', async (req, res) => {  //http://localhost:3001/api/users/login
+    // Check if the user is already logged in
+    if (req.session.logged_in) {
+        if (req.headers.accept === 'application/json') {
+            res.status(400).json({ message: 'You are already logged in!' });
+        } else {
+            res.redirect('/homepage');
+        }
+        return;
+    }
+
     try {
-        const userData = await User.findOne({ where: { name: req.body.name } });
+        // Find user by email
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
         if (!userData) {
-            res.status(400).json({ message: 'Incorrect name or password, please try again' });
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
             return;
         }
+
         const validPassword = await userData.checkPassword(req.body.password);
+
         if (!validPassword) {
-            res.status(400).json({ message: 'Incorrect name or password, please try again' });
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
             return;
         }
+
         req.session.save(() => {
             req.session.user_id = userData.id;
             req.session.logged_in = true;
-            res.json({ user: userData, message: 'You are now logged in!' });
+
+            // Check if the request is from Insomnia (Accept header is set to 'application/json')
+            if (req.headers.accept === 'application/json') {
+                res.json({ user: userData, message: 'You are now logged in!' });
+            } else {
+                // Redirect the user to the homepage after successful login
+                // res.redirect('/homepage');
+                res.json({ user: userData, message: 'You are now logged in!' });
+            }
         });
-        res.render('homepage', {
-            logged_in: req.session.logged_in,
-        });
+
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
+router.delete('/logout/:id', async (req, res) => { //http://localhost:3001/api/users/logout/1
+    try {
+        const targetUserId = parseInt(req.params.id, 10);
 
+        if (req.session.user_id !== targetUserId) {
+            res.status(403).json({ message: 'You can only log out of your own session' });
+            return;
+        }
+
+        if (req.session.logged_in) {
+            req.session.destroy(() => {
+                res.status(204).json({ message: 'You are now logged out!' }).end();
+            });
+        } else {
+            res.status(404).json({ message: 'No session found' }).end();
+        }
+    } catch (err) {
+        res.status(500).json({ message: 'An error occurred', error: err });
+    }
+});
+
+//BELLOW ARE DEBUG ROUTES
 
 //debug route for viewing all users
-router.get('/debug', async (req, res) => { //http://localhost:3001/api/users/deubg
+router.get('/debug', async (req, res) => { //http://localhost:3001/api/users/debug
     try {
         const allUsers = await User.findAll();
         res.status(200).json(allUsers);
@@ -83,5 +127,25 @@ router.get('/debug', async (req, res) => { //http://localhost:3001/api/users/deu
     }
 });
 
+router.get('/debug/open-sessions', async (req, res) => { //http://localhost:3001/api/users/debug/open-sessions
+    try {
+        const sessionCount = await sequelize.models.Session.count(); //get the number of open sessions
+        res.status(200).json({ openSessions: sessionCount }); //return the number of open sessions
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
+router.get('/debug/logout-all', async (req, res) => { //http://localhost:3001/api/users/debug/logout-all
+    try {
+        await sequelize.models.Session.destroy({ where: {} });
+        res.status(200).json({ message: 'All users have been logged out' });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+//DEBUG ROUTES END
+
+
+//Export the router
 module.exports = router;
