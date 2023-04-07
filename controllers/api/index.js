cc99c3d04587e7cb136863db817f6dc6af8c5a53
ttieman <tiemantanner@gmail.com>
2023-04-06 21:11:05 -0700

    created create poll route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const pollRoutes = require('./pollRoutes')
 // const projectRoutes = require('./projectRoutes');
-
+router.use('/poll', pollRoutes);
 router.use('/users', userRoutes);
 // router.use('/projects', projectRoutes);
 
