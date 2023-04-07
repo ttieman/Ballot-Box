6bd31c7948fd5f4856b19729d9672c0bae66b6f7
CF <cfreitas12345@gmail.com>
2023-04-07 00:45:50 -0700

    Coded login, signup, poll.js and the hbs.

--- public/js/poll-create.js
+++ public/js/poll-create.js
@@ -1,1 +1,25 @@
 
+module.exports = (sequelize, DataTypes) => {
+  const Form = sequelize.define('Form', {
+    question: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    answerA: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    answerB: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    answerC: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  });
+
+  return Form;
+};
+
+
