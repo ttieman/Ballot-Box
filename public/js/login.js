b0fc4460985679327aa7ae2b5a33b2155e222a6e
arishorts <arieljschwartz@gmail.com>
2023-04-08 12:42:01 -0700

    Login and Logout routes now render pages and have functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,27 +1,26 @@
-
 const loginFormHandler = async (event) => {
   event.preventDefault();
-  const username = document.querySelector('#username-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
+  const username = document.querySelector("#username-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
 
   if (username && password) {
-    const response = await fetch('/api/users/login', {
-      method: 'POST',
+    const response = await fetch("/api/users/login", {
+      method: "POST",
       body: JSON.stringify({ username, password }),
-      headers: { 'Content-Type': 'application/json' },
+      headers: { "Content-Type": "application/json" },
     });
 
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace("/");
     } else {
-      alert('Failed to log in');
+      alert("Failed to log in");
     }
   }
 };
 
 document
-  .querySelector('.login-form')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
 
 // const loginFormHandler = async (event) => {
 //   event.preventDefault();
