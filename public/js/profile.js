f40b15f9f17f4d0c4443731bba4e5038e3e84052
arishorts <arieljschwartz@gmail.com>
2023-04-11 13:00:55 -0700

    Delete buttons work on profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,37 +1,107 @@
+const choicesContainer = document.getElementById("choices-container");
+const numChoicesInput = document.getElementById("num-choices");
+const user_id = window.location.href.split("/").pop();
+
+const deletePostHandler = async (event, button) => {
+  event.preventDefault();
+  const poll_id = button.dataset.delete;
+  const response = await fetch(`/api/poll/delete/${poll_id}`, {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace(`/profile/${user_id}`);
+  } else {
+    alert("Failed to delete");
+  }
+};
+
 const addPostHandler = () => {
-  console.log("hello");
   document.querySelector("#addPollContainer").style.display = "block";
   document.querySelector("#pollsContainer").style.display = "none";
   document.querySelector("#addPoll").style.display = "none";
 };
 
-const postFormHandler = async (event) => {
-  event.preventDefault();
-  const title = document.querySelector("#title-poll").value.trim();
-  const description = document.querySelector("#description-poll").value.trim();
-  const user_id = window.location.href.split("/").pop();
+document
+  .getElementById("new-poll-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-  if (title && description) {
-    const response = await fetch("/dashboard", {
+    const question = document.getElementById("question").value.trim();
+    const numChoices = parseInt(document.getElementById("num-choices").value);
+    const choicesContainer = document.getElementById("choices-container");
+
+    const answers = [];
+    for (let i = 0; i < numChoices; i++) {
+      const answerInput = choicesContainer.querySelector(
+        `input[name="choice-${i}"]`
+      );
+      if (answerInput) {
+        answers.push(answerInput.value);
+      }
+    }
+
+    const pollData = {
+      question,
+      answers,
+    };
+
+    const response = await fetch("/api/poll/create", {
       method: "POST",
-      body: JSON.stringify({ title, description }),
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pollData),
     });
 
     if (response.ok) {
-      document.location.replace(`/dashboard/${user_id}`);
+      document.location.replace(`/profile/${user_id}`);
     } else {
-      alert("Failed to post");
+      alert("Error creating poll");
     }
+  });
+
+function createChoiceInputs(numChoices) {
+  choicesContainer.innerHTML = "";
+  for (let i = 0; i < numChoices; i++) {
+    const choiceLabel = document.createElement("label");
+    choiceLabel.innerHTML = `Choice ${i + 1}:`;
+    choiceLabel.htmlFor = `choice-${i}`;
+    choiceLabel.classList.add("input-group-text");
+
+    const choiceInput = document.createElement("input");
+    choiceInput.type = "text";
+    choiceInput.id = `choice-${i}`;
+    choiceInput.name = `choice-${i}`;
+    choiceInput.required = true;
+    choiceInput.classList.add("form-control");
+
+    const choiceInputGroup = document.createElement("div");
+    choiceInputGroup.classList.add("input-group", "mb-3");
+    choiceInputGroup.appendChild(choiceLabel);
+    choiceInputGroup.appendChild(choiceInput);
+    choicesContainer.appendChild(choiceInputGroup);
   }
-};
+}
+
+numChoicesInput.addEventListener("input", (event) => {
+  const numChoices = parseInt(event.target.value, 10);
+  if (numChoices > 0) {
+    createChoiceInputs(numChoices);
+  } else {
+    choicesContainer.innerHTML = "";
+  }
+});
+createChoiceInputs(2);
 
 document.querySelector("#addPollContainer").style.display = "none";
 document.querySelector("#pollsContainer").style.display = "block";
 document.querySelector("#addPoll").style.display = "block";
 
-document
-  .querySelector(".poll-form")
-  .addEventListener("submit", postFormHandler);
-
 document.querySelector("#addPoll").addEventListener("click", addPostHandler);
+
+const deletePollButtons = document.querySelectorAll(".deletePoll");
+deletePollButtons.forEach((button) => {
+  button.addEventListener("click", (event) => deletePostHandler(event, button));
+});
